4e88ac7c98e4b3b1aeb5655cb37a849dd5cdfe79
anujmishra94666 <123732695+anujmishra94666@users.noreply.github.com>
2023-07-06 10:47:58 +0530

    Initial commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,106 @@
+const Menu = [
+  {
+    id: 1,
+    image:
+      "https://th.bing.com/th/id/OIP.Yd-d23HK6T6OysNic2ZAEQHaFg?w=223&h=180&c=7&r=0&o=5&pid=1.7",
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "Maggi is the quick-fix solution for the middle class Indian. Got no time to make food? Utilise 2-5 minutes to make your favourite Maggi noodles. Its price point and flavour have made it the staple food of every Indian household. So how did this brand gain popularity in India? "
+  },
+
+  {
+    id: 2,
+    image:
+      "https://th.bing.com/th/id/OIP.71VpoBVMKJgOtnSlAXvgeAHaLG?w=226&h=339&c=7&o=5&pid=1.7",
+    name: "allu pakoida",
+    category: "evening",
+    price: "20₹",
+    description:
+      "Aloo Pakora also known as potato pakora and aloo bajji in the South, are Indian style potato fritters made with sliced potatoes, gram flour and spices. "
+  },
+  {
+    id: 3,
+    image:
+      "https://th.bing.com/th/id/OIP.n0vrsTO-TFMOAY53Hh5QgwHaFj?w=222&h=180&c=7&r=0&o=5&pid=1.7",
+    name: "corn",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "Maize, also known as corn in North American and Australian English, is a cereal grain first domesticated by indigenous peoples in southern Mexico about 10,000 years ago"
+  },
+  {
+    id: 4,
+    image:
+      "https://th.bing.com/th/id/OIP.hh8DbHqWjeZb6qDJmM9NtgAAAA?w=320&h=180&c=7&r=0&o=5&pid=1.7",
+    name: "chola",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "Chola's Foods’ the essence from the western ghats of India, brings you a complete range of holistic organic dietary supplements to enhance the quality of your lives. The role of quality and hygiene of traditional (parampara) and ethnic foods has been increased in their consumption pattern with increasing demand from population."
+  },
+  {
+    id: 5,
+    image:
+      "https://th.bing.com/th/id/OIP.sQkPIy7t415jq1UvjKEmnwHaJQ?w=144&h=180&c=7&r=0&o=5&pid=1.7",
+    name: "pizza",
+    category: "evening",
+    price: "80₹",
+    description:
+      "a dish of Italian origin, consisting of a flat round base of dough baked with a topping of tomatoes and cheese, typically with added meat, fish, or vegetables:. "
+  },
+  {
+    id: 6,
+    image:
+      "https://th.bing.com/th/id/OIP.pmm1loGXR0BnlWAB_FHK7QHaHa?w=159&h=180&c=7&r=0&o=5&pid=1.7",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Non-vegetarian food is a term commonly used in India for food that has meat, fish, or eggs in it. It is more commonly called non-veg food. Not just for food, it can be used for a person too - a person eating meat is a non-vegetarian in Indian English. Egg too is not classified as vegetarian for Indians.. "
+  },
+  {
+    id: 7,
+    image:
+      "https://th.bing.com/th/id/OIP.pYfay5OqSg0f6HAyQh8VKgHaE8?w=282&h=188&c=7&r=0&o=5&pid=1.7",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "There is so many sweets in our india like ladoo , gulab jamun etc. "
+  },
+  {
+    id: 8,
+    image:
+      "https://th.bing.com/th/id/OIP.UF-wj0zvXyFLpXFBw6gHvQHaEK?w=307&h=180&c=7&r=0&o=5&pid=1.7",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60₹",
+    description:
+      "Rajma chawal is a traditional Indian dish made with red kidney beans and cooked rice. It is a complete vegetarian meal that is suitable for lunch or dinner. "
+  },
+  {
+    id: 9,
+    image:
+      "https://th.bing.com/th?q=1+Samosa&w=120&h=120&c=1&rs=1&qlt=90&cb=1&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247",
+    name: "samaso",
+    category: "evening",
+    price: "10₹",
+    description:
+      "Samosa is a fried pastry that has a triangular shape and a spiced filling. The filling can be made with vegetables, such as potatoes, peas, carrots, onion, and coriander, or with meat, such as lamb or chicken. It is a common snack in India and some neighboring countries "
+  },
+
+  {
+    id: 10,
+    image:
+      "https://th.bing.com/th/id/OIP.jHVOWwSFJmKm4WYjoOIS0QHaFk?w=252&h=189&c=7&r=0&o=5&pid=1.7",
+    name: "gulab jamun",
+    category: "desert",
+    price: "10₹",
+    description:
+      "In the Indian subcontinent, milk and cheese solids are prepared by heating milk over a low flame for a long time until the water content has evaporated and only the milk solids remain.   "
+  }
+];
+
+export default Menu;
